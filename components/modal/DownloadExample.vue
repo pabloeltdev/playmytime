2c20da4b5cd0bff68185deadc4b453de072119ca
Pablo E. L. Targino <pabloeltdev@proton.me>
2024-03-06 12:23:58 -0300

    Fixing relative url

--- components/modal/DownloadExample.vue
+++ components/modal/DownloadExample.vue
@@ -25,7 +25,7 @@ defineProps<{
         how to create your own playlist
       </p>
       <Button>
-        <NuxtLink to="files/playlist.zip" target="_blank" external>Download playlist</NuxtLink>
+        <NuxtLink to="../files/playlist.zip" target="_blank" external>Download playlist</NuxtLink>
       </Button>
     </div>
   </Modal>
